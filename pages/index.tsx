0ad085e86a3dfe20fdeb4e58e96b51f22d726311
Paweł Ciosek <ciosek.pawel@gmail.com>
2023-05-16 21:55:42 +0200

    bump deps

--- pages/index.tsx
+++ pages/index.tsx
@@ -3,7 +3,7 @@ import type { NextPage } from 'next'
 const Home: NextPage = () => {
   return (
     <div>
-      <h1 className="text-3xl text-red-600">Hello world</h1>
+      <h1 className="text-3xl text-red-600">Hello world with tailwindcss styles!</h1>
     </div>
   )
 }
